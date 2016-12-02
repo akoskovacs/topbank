c0b176f7ef92f430cbeff37f50be5f9592e237df
Ákos Kovács <akoskovacs@gmx.com>
2016-12-02 23:39:20 +0100

    Ruby code deleted

--- models/all.rb
+++
@@ -1,8 +0,0 @@
-require 'mysql2'
-
-require './models/DBModel.rb'
-require './models/Fiok.rb'
-require './models/Szamla.rb'
-require './models/Kartya.rb'
-require './models/Ugyfel.rb'
-require './models/Tranzakcio.rb'
